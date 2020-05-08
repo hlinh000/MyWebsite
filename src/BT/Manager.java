d81e87c2d91135c3f48b965cdac0142582eeb2d6
VYHUYNH <huynhvy612@gmail.com>
2020-05-08 21:11:21 +0700

    được chưa

---
+++ src/BT/Manager.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BT;
+
+/**
+ *
+ * @author Laptop88
+ */
+public class Manager {
+    public static void menu() {
+        System.out.println("========================================");
+        System.out.println("|                Menu Demo             |");
+        System.out.println("========================================");
+        System.out.println("|             1.Create                 |");
+        System.out.println("|             2.Find and Sort          |");
+        System.out.println("|             3.Update / Delete        |");
+        System.out.println("|             4.Report                 |");
+        System.out.println("|             5.Exit                   |");
+        System.out.println("========================================");
+    }
+}
